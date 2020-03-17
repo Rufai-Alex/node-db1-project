c8807046fe7e724f29fe7f4d1588e751aef78ebf
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-03-17 09:59:53 +0100

    knex

--- api/server.js
+++ api/server.js
@@ -6,4 +6,14 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", async (req, res) => {
+  try {
+    const data = await getAll();
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: " err from get all" });
+  }
+});
+
 module.exports = server;
